6de0be7e7846d523cc1360bc0a9724eee054bfa9
Tianxing Li <li-tianxing@hotmail.com>
2024-05-09 21:03:41 +0800

    -

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,8 +1,8 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:C:\Users\labuser\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy@v0.0.0-20210425191248-0afcc81296de\assets\js\wowchemy-utils.js
+  // ns-hugo:C:\Users\li-ti\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy@v0.0.0-20210425191248-0afcc81296de\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
